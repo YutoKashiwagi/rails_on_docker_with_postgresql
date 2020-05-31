c57c42cdfbb9823b5b02ba547a52dbc2e3eae79c
YutoKashiwagi <kashiwagi19950714@gmail.com>
2020-05-31 18:18:20 +0900

    画像投稿機能実装

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -69,6 +69,6 @@ class TweetsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def tweet_params
-      params.require(:tweet).permit(:content)
+      params.require(:tweet).permit(:content, :tweet_image)
     end
 end
